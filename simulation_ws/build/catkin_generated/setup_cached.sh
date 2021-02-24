aa79c4f8206916491bce2f1135581b777083cb4c
kiki-sarpong <kikiyawsarpong@gmail.com>
2021-02-23 23:43:42 -0500

    simulation updates

--- simulation_ws/build/catkin_generated/setup_cached.sh
+++ simulation_ws/build/catkin_generated/setup_cached.sh
@@ -7,9 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH='/home/kiki/github_files/simulation_ws/devel:/opt/ros/melodic'
-export LD_LIBRARY_PATH='/home/kiki/github_files/simulation_ws/devel/lib:/opt/ros/melodic/lib'
-export PKG_CONFIG_PATH='/home/kiki/github_files/simulation_ws/devel/lib/pkgconfig:/opt/ros/melodic/lib/pkgconfig'
 export PWD='/home/kiki/github_files/simulation_ws/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/kiki/github_files/simulation_ws/devel/share/common-lisp'
-export ROS_PACKAGE_PATH='/home/kiki/github_files/simulation_ws/src:/opt/ros/melodic/share'
